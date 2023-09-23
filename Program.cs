8d42cdcc9378d358a2240ab3139f8c675ac17004
Ekaterina Borisova <ekd11222@outlook.com>
2023-09-24 01:00:28 +0400

    Задача 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// <Ctrl+K>, <Ctrl+C>, чтобы "закомментировать" блок кода, и <Ctrl+K>, <Ctrl+U> - чтобы "раскомментировать" его.
+
+//                                                         Задача 4: 
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.WriteLine("Введите числa: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+double max;
+    if (a > b)
+    {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+ 
+    if (c > max)
+    {
+        max = c;
+    }
+
+Console.WriteLine($"Максимальное значение: {max}");
