9bdfe2fc5991f899f24446f58d7d28fb78dc50dc
Ekaterina Borisova <ekd11222@outlook.com>
2023-09-24 02:03:37 +0400

    Задача 8

--- Program.cs
+++ Program.cs
@@ -88,3 +88,20 @@
 //      {
 //         System.Console.WriteLine ($"Число {a} не делится на 2 без остатка -> нечетное");
 //     }
+
+
+
+
+
+//                                                                 Задача 8
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("N = ");
+int a = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=a;i++)
+    {
+        if (i % 2 == 0)
+        System.Console.WriteLine($"{i}");
+    }
